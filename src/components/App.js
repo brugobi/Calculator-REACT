6231354a1307a8f32aeea585c3781337ae9ef116
brugobi <brgobbi@hotmail.com>
2021-03-15 17:18:39 -0400

    replace App function to a class

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,28 @@
 import '../App.css';
+import { Component } from 'react';
 import Display from './Display';
 import ButtonPannel from './ButtonPanel';
 /* eslint-disable no-unused-vars */
 import Calculate from '../logic/calculate';
 
-const App = () => (
-  <>
-    <div>
-      <Display />
-      <ButtonPannel />
-    </div>
-  </>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  render() {
+    <>
+      <div>
+        <Display />
+        <ButtonPannel />
+      </div>
+    </>;
+  }
+}
 
 export default App;
