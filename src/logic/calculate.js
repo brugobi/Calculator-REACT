2cac638cde824861697385641e9ee4d9a3a0398c
brugobi <brgobbi@hotmail.com>
2021-03-15 13:07:22 -0400

    add more cases to the logic

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -4,6 +4,21 @@ const calculate = (calculatorData, btnName) => {
   const { total, next, operation } = calculatorData;
 
   switch (btnName) {
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+      if (operation) {
+        return { total, next: next ? next + btnName : btnName, operation };
+      }
+      return { total: total ? total + btnName : btnName, next, operation };
+
     case 'AC':
       return { total: null, next: null, operation: null };
 
