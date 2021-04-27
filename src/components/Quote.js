de7be0b5672cbfd76d19df3405ea0fdad652038b
brugobi <brgobbi@hotmail.com>
2021-04-27 12:37:03 -0400

    add quote content

---
+++ src/components/Quote.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Quote() {
+  return (
+    <div>
+      <p>
+        &quot;Why Do We Fall, Sir? So That We Can Learn To Pick Ourselves Up.&quot; -
+        <i>Batman Begins</i>
+      </p>
+    </div>
+  );
+}
+
+export default Quote;
