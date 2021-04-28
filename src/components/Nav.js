503ac6736ea265936c3808b707ea7e6ca289988b
brugobi <brgobbi@hotmail.com>
2021-04-28 13:08:18 -0400

    finished styling the Nav and add an image to the README

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,23 +3,23 @@ import { Link } from 'react-router-dom';
 
 function Nav() {
   return (
-    <nav className="navbar is-transparent" role="navigation" aria-label="main navigation">
+    <nav className="navbar" role="navigation" aria-label="main navigation">
 
       <div className="navbar-brand">
         <div className="navbar-item">
-          <div className="brand">Math Magicians</div>
+          <div className="brand font-orange font20px ZenDotsFont">Math Magicians</div>
         </div>
       </div>
 
-      <div className="navbar-end">
+      <div className="navbar-end font-orange ZenDotsFont">
         <div className="navbar-item">
-          <Link to="/">Home</Link>
+          <Link to="/" className="nav-link font-orange">Home</Link>
         </div>
         <div className="navbar-item">
-          <Link to="/calculator">Calculator</Link>
+          <Link to="/calculator" className="nav-link font-orange">Calculator</Link>
         </div>
         <div className="navbar-item">
-          <Link to="/quote">Quote</Link>
+          <Link to="/quote" className="nav-link font-orange">Quote</Link>
         </div>
       </div>
     </nav>
